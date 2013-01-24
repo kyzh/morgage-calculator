f5342b6fc5a2cf35766e406fef6d36ce9932ed0c
Florentin Raud <florentin.raud@gmail.com>
2013-01-24 12:31:19 +0000

    Added the monthly paiment breakdown back

--- morgage.rb
+++ morgage.rb
@@ -50,15 +50,17 @@ class Morgage
     puts ""
     puts "The amount towards deposit:       #{@deposit}"
     puts "The amount borrowed:              #{@principal}"
-    puts "The amount of interest add:       #{@interest_paid.round}"
     puts ""
-    puts "The amount for stamp duty add :   #{@stamp_duty}"
-    puts "The amount for survey add:        #{@survey}"
-    puts "The amount for conveyancing add:  #{@conveyancing}"
+    puts "Iterest cost:                     #{@interest_paid.round}"
+    puts "Stamp duty cost:                  #{@stamp_duty}"
+    puts "Survey cost:                      #{@survey}"
+    puts "Conveyancing cost:                #{@conveyancing}"
+    puts "Total cost:                       #{@overall_cost.round}"
     puts ""
-    puts "Upfront requirement:              #{@upfront_paid}"
-    puts "All non capital cost:             #{@overall_cost.round}"
     puts "Overall budget:                   #{@overall_spending.round}"
+    puts "Upfront requirement:              #{@upfront_paid}"
+    puts ""
+    puts "Monthly breakdown:"
     @repaiments.each{|repaiment| puts "Month #{repaiment["month"]}: capital: #{repaiment["capital"].round} interest: #{repaiment["interest"].round} repaiment #{(repaiment["capital"] + repaiment["interest"]).round}) balance : #{repaiment["balance"].round}"}
     #puts "#{current_month} : repayment is #{@monthly_repayment.round} ( #{monthly_capital.round} #{monthly_interest.round}) balance : #{@loan_balance.round}"
   end
@@ -67,7 +69,7 @@ class Morgage
     @monthly_interest   = (@interest / (12 * 100)).round(6)
     @monthly_repayment  = @principal * ( @monthly_interest / (1 - (1 + @monthly_interest) ** -@amortisment_length))
     @amortisment_length.times do |m|
-      @repaiments << {"month" => m ,"interest"=> @loan_balance * @monthly_interest, "capital"=> @monthly_repayment - @loan_balance * @monthly_interest, "balance" => @monthly_repayment - @loan_balance * @monthly_interest}
+      @repaiments << {"month" => m ,"interest"=> @loan_balance * @monthly_interest, "capital"=> @monthly_repayment - @loan_balance * @monthly_interest, "balance" => @loan_balance - @monthly_repayment}
       @loan_balance -= @monthly_repayment - @loan_balance * @monthly_interest
     end
     @capital_paid     = @repaiments.collect{|m| m["capital"]}.inject(:+)
